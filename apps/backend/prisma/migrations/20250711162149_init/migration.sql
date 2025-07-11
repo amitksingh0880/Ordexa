472f1c6ddb5006822f751ea623f23e3557c95e50
Amit Singh <amitksingh.1204@gmail.com>
2025-07-11 21:56:27 +0530

    feat: base boilerplate setup (bun, express, prisma, postgres)

---
+++ apps/backend/prisma/migrations/20250711162149_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "totalAmount" DECIMAL(65,30) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OutboxEvent" (
+    "id" TEXT NOT NULL,
+    "aggregateId" TEXT NOT NULL,
+    "aggregateType" TEXT NOT NULL,
+    "eventType" TEXT NOT NULL,
+    "payload" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "processed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "OutboxEvent_pkey" PRIMARY KEY ("id")
+);
